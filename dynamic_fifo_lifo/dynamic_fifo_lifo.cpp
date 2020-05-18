8ea437a962956318eff2a81f91d1c274eaaffff0
Diego Bahena <dbahenalopez@hotmail.com>
2020-05-18 01:53:50 -0500

    dynamic fifo lifo functional

--- dynamic_fifo_lifo/dynamic_fifo_lifo.cpp
+++ dynamic_fifo_lifo/dynamic_fifo_lifo.cpp
@@ -13,7 +13,7 @@ public:
 	fifo_lifo* fifo(fifo_lifo*, fifo_lifo*, int);
 	void  lifo(fifo_lifo*, fifo_lifo*, int);
 	void insert( fifo_lifo*, fifo_lifo*, int);
-	void show();
+	void show(fifo_lifo*, fifo_lifo*, int);
 	int age;
 	char name[30];
 	fifo_lifo* Next;
@@ -24,22 +24,29 @@ public:
 
 
 
-//The method insert adds 1 element to the array and if the array is full, gives a message
+//this is the function where we will add the data to our "array"
  void fifo_lifo::insert( fifo_lifo * Beginning,fifo_lifo * Cursor, int counter) {
 
+	 //The variable counter is a flag to know if the elements introduced are the first introduced in the array
 	 if (counter == 0) {  
 
+
+		 //the elements are assigned to the pointer Beginning; Beginning is literally, the beginning of the array
 		cout << "Ingresa el nombre" << endl;
 		cin.ignore();
 		cin.getline( Beginning->name, 30, '\n');
 
 		cout << "Ingresa la edad" << endl;
 		cin >>Beginning->age;
+
+		//we must be sure of don't skip this code line, where the pointer Next isn't pointing to an address right now
 		Beginning->Next = NULL;
 
 	 }
 	 else {	
 
+
+		 // starting in the second group of elements to our array, we add them to the pointer Temp
 		fifo_lifo* Temp = new fifo_lifo;
 
 		cout << "Ingresa el nombre" << endl;
@@ -52,6 +59,8 @@ public:
 		
 		Cursor = Beginning;
 
+
+		//this cycle is like we use a for with the ++ increment
 		while (Cursor->Next != NULL) {
 
 			Cursor = Cursor->Next;
@@ -66,23 +75,53 @@ public:
 
 }
 
-//the method show prints the array, and if the array is empty, sends a message
-void fifo_lifo::show(fifo_lifo* Beginning, fifo_lifo*, int) {
+
+void fifo_lifo::show(fifo_lifo* Beginning, fifo_lifo* Cursor, int counter) {
 	
+
+	//if there is only one group of elements, we know that elements are in the ponter beginning
+
+	if (counter == 1) {
+
+
+		//Cursor will be equal to beginning
+		Cursor = Beginning;
+		cout << Cursor->name <<'\t'<< Cursor->age << endl;
+	}
+	//starting in the second group of elements to our array, we print the elements in Beginning, but also in the rest of the array
+	else {
+
+		Cursor = Beginning;
+		cout << Cursor->name <<'\t' << Cursor->age << endl;
+		Cursor = Cursor->Next;
+
+		//the cylce ++, if we think statically (like the teacher said in the pdf :) )
+		while (Cursor->Next != NULL) {
+
+			cout << Cursor->name <<'\t'<< Cursor->age << endl;
+			Cursor = Cursor-> Next;
+		}
+		cout << Cursor->name << '\t' << Cursor->age << endl;
+	}
+
+
+	system("Pause");
 
 }
 
 
 
-//lifo just takes the last element and change it for 0
+//the last in first out function
 void fifo_lifo::lifo(fifo_lifo* Beginning, fifo_lifo* Cursor, int counter) {
 
+	//we need the cursor to point the beginning of the array
 	Cursor = Beginning;
+	//an extra flag to know more exactly the positons of each element
 	int marker = 1;
 
 	while (Cursor->Next != NULL) {
 		
-		if (marker == counter - 1) {
+		if (marker == counter-1) {
 			
 			fifo_lifo* Temp = new fifo_lifo;
 
@@ -93,21 +132,34 @@ void fifo_lifo::lifo(fifo_lifo* Beginning, fifo_lifo* Cursor, int counter) {
 		else {
 			Cursor = Cursor->Next;
 		}
-		marker;
+		marker++;
 	}
-	delete Cursor;//Borramos cursor que es la ultima "posicion"
+
+	//now we delete the content of cursor, because it was the last element
+	delete Cursor; 
 }
 
 
 
 
-
-
-
-
-//fifo structure makes the array move to a new position where the first element is deleted
-fifo_lifo* fifo_lifo::fifo() {
+//the first in first out function
+fifo_lifo* fifo_lifo::fifo(fifo_lifo* Beginning , fifo_lifo* Cursor, int counter ) {
 	
+	//we save memory for temp
+	fifo_lifo* Temp = new fifo_lifo;
+
+
+	//point it to the beginning of the array
+	Temp = Beginning;
+	//move the pointer beginning
+	Beginning = Beginning->Next;
+	
+
+	//now beginning is the second element & we delete the first (Temp)
+	delete Temp;
+	
+	
+	return Beginning;
 
 }
 
@@ -133,11 +185,23 @@ int main() {
 		case 1:
 
 			Beginning->insert(Beginning, Cursor, counter);
+
+			//we need this flag to know how many elements exists
 			counter++;
 			break;
 
 		case 2:
-		
+
+			if (counter == 0) {
+
+				cout << "Todavía no hay ningún elemento que eliminar" << endl;
+				system("Pause");
+			}
+			else {
+
+				Beginning = Beginning->fifo(Beginning, Cursor, counter);
+				counter--;
+			}
 
 		break;
 
@@ -145,6 +209,7 @@ int main() {
 			if (counter == 0) {
 
 				cout << "Todavía no hay ningún elemento que eliminar" << endl;
+				system("Pause");
 			}
 			else {
 
@@ -156,12 +221,19 @@ int main() {
 			  break;
 
 		case 4: 
+				cout << "\n";
 
-			
+				if (counter == 0) {
+					cout <<"Todavía no hay nada que mostrar"<<endl;
+					system("Pause");
+				}
 
-		
+				else {
+					Beginning->show (Beginning, Cursor, counter);
+				}
+				break;
 
-			  break;
+
 
 		case 5:
 
